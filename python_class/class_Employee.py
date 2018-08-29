88afe3d9c3c269cd82b93e258a6a985cc4c27bfc
BlessedChild <2250970846@qq.com>
2018-08-29 13:58:27 +0800

    update

---
+++ python_class/class_Employee.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# v2.7
+# macOS10.13.6
+class Employee:
+   empCount = 0
+ 
+   def __init__(self, name, salary):
+      self.name = name
+      self.salary = salary
+      Employee.empCount += 1
+   
+   def displayCount(self):
+     print "Total Employee %d" % Employee.empCount
+ 
+   def displayEmployee(self):
+      print "Name : ", self.name,  ", Salary: ", self.salary
---
+++ python_class/show_Employee.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# v2.7
+# macOS10.13.6
+
+from class_Employee import Employee
+
+xiaomi = Employee('xiaomi',8000)
+
+xiaomi.displayEmployee()
